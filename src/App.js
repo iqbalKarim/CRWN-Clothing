94b9eb0babd287f78dce8de930fd10256333294c
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2022-02-04 01:56:48 +0500

    Router init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const Hats = () => {
+  return (
+    <div>
+      <h1>Hats</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path={'/'} component={HomePage} />
+      <Route exact path={'/shop/hats'} component={Hats} />
     </div>
   );
 }
