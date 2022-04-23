a158c8b11e03c6a8b4e5763a033db03604f33b78
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2022-04-23 03:17:44 +0300

    refractoring

--- src/App.js
+++ src/App.js
@@ -1,48 +1,12 @@
-import { Component } from 'react/cjs/react.production.min';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/home/home.component';
 
-import Header from './components/header/header.component';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
-import { auth } from './firebase/firebase.utils';
-
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-          <Route exact path={'/'} component={HomePage} />
-          <Route exact path={'/shop'} component={ShopPage} />
-          <Route exact path={'/signin'} component={SignInAndSignUp} />
-        </Switch>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' index element={<Home />} />
+    </Routes>
+  );
+};
 
 export default App;
